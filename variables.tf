f33d67ac7a802abcde814ed46265e466996deddc
dpeacock <dpeacock@hashicorp.com>
2022-04-14 16:27:54 +0000

    Added network module v2

--- variables.tf
+++ variables.tf
@@ -74,3 +74,6 @@ variable "placeholder" {
   description = "Image-as-a-service URL. Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
 }
 
+variable "resource_group_name" {
+    default = "dap-test-rg-name"
+}
